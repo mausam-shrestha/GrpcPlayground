6b83cdb196029d5115af34f847923a119b3a2c4d
Mausam <46900712+mausamshresthabv@users.noreply.github.com>
2022-05-30 23:25:42 +0100

    Matches the proto file to server for client and calls the unary rpc to change string case.

--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -4,9 +4,22 @@ using GrpcGreeterClient;
 
 // The port number must match the port of the gRPC server.
 using var channel = GrpcChannel.ForAddress("https://localhost:7110");
+
 var client = new Greeter.GreeterClient(channel);
-var reply = await client.SayHelloAsync(
-                  new HelloRequest { Name = "GreeterClient" });
-Console.WriteLine("Greeting: " + reply.Message);
-Console.WriteLine("Press any key to exit...");
-Console.ReadKey();
+
+do
+{
+    Console.WriteLine("Input your string human:");
+
+    var rawString = Console.ReadLine();
+
+    var reply = await client.ChangeToLowercaseAsync(new RawStringRequest { RawString = rawString });
+
+    Console.WriteLine($"Greeter service says: { reply.RawString }");
+
+    Console.WriteLine("Press Q to quite or any othe key to continue.");
+
+} while (Console.ReadKey().Key != ConsoleKey.Q);
+
+
+
